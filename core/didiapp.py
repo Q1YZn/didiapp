541fd9f00be578bd22e18f7de03544f9ef3aa38a
Q1YZn <2019000102494@protonmail.com>
2024-04-04 21:53:09 +0800

    add tommorrow task

--- core/didiapp.py
+++ core/didiapp.py
@@ -21,14 +21,18 @@ def get_didiapp_info(api, token):
 
     return reponse_json
 
-def send_ding_talk(boy,girl):
+def send_ding_talk(boy,girl,tomboy,tomgirl):
     # WebHook地址
-    webhook = 'https://oapi.dingtalk.com/robot/send?access_token=？'
+    webhook = 'https://oapi.dingtalk.com/robot/send?access_token='
     # secret = 'SEC11b9...这里填写自己的加密设置密钥'  # 可选：创建机器人勾选“加签”选项时使用
     # 初始化机器人小丁
     xiaoding = DingtalkChatbot(webhook)  # 方式一：通常初始化方式
     # xiaoding = DingtalkChatbot(webhook)  # 方式二：勾选“加签”选项时使用（v1.5以上新功能）
     # xiaoding = DingtalkChatbot(webhook, pc_slide=True)  # 方式三：设置消息链接在PC端侧边栏打开（v1.5以上新功能）
     # Text消息@所有人
-    send_data = f"今天的男方任务是:{boy.task}\r\n跳转链接:{boy.link}\r\ncodeinfo是:{boy.code_info}\r\nlevel是:{boy.level}\r\n\r\n今天的女方任务是:{girl.task}\r\n跳转链接:{girl.link}\r\ncodeinfo是:{girl.code_info}\r\nlevel是:{girl.level}"
+    send_data = (f"今天的男方任务是:{boy.task}\r\n跳转链接:{boy.link}\r\ncodeinfo是:{boy.code_info}\r\nlevel是:{boy.level}\r\n\r\n"
+                 f"今天的女方任务是:{girl.task}\r\n跳转链接:{girl.link}\r\ncodeinfo是:{girl.code_info}\r\nlevel是:{girl.level}\r\n\r\n"
+                 f"明天的男方任务是:{tomboy.task}\r\n跳转链接:{tomboy.link}\r\ncodeinfo是:{tomboy.code_info}\r\nlevel是:{tomboy.level}\r\n\r\n"
+                 f"明天的女方任务是:{tomgirl.task}\r\n跳转链接:{tomgirl.link}\r\ncodeinfo是:{tomgirl.code_info}\r\nlevel是:{tomgirl.level}\r\n\r\n"
+                 )
     xiaoding.send_text(msg=send_data, is_at_all=False)
--- main.py
+++ main.py
@@ -41,8 +41,10 @@ def deal_time(today_task_time):
 if __name__ == '__main__':
     boy = Employee
     girl = Employee
+    tomboy = Employee
+    tomgirl = Employee
     get_task_info_api = "https://api.didiapp.com:443/client/challenge/info/"
-    token = "？"
+    token = ""
     retData = get_didiapp_info(get_task_info_api, token)
 
     print(retData)
@@ -61,11 +63,21 @@ if __name__ == '__main__':
     boy.code_info = retData["challenge_info"]["boy_next_task_info"]["code"]
 
     girl.task = retData["challenge_info"]["girl_next_task_info"]["profile"]
-
     girl.link = retData["challenge_info"]["girl_next_task_info"]["link"]
     girl.level = retData["challenge_info"]["girl_next_task_info"]["level"]
     girl.code_info = retData["challenge_info"]["girl_next_task_info"]["code"]
 
+    tomboy.task = retData["challenge_info"]["boy_tomorrow_task_info"]["profile"]
+    tomboy.link = retData["challenge_info"]["boy_tomorrow_task_info"]["link"]
+    tomboy.level = retData["challenge_info"]["boy_tomorrow_task_info"]["level"]
+    tomboy.code_info = retData["challenge_info"]["boy_tomorrow_task_info"]["code"]
+
+    tomgirl.task = retData["challenge_info"]["girl_tomorrow_task_info"]["profile"]
+    tomgirl.link = retData["challenge_info"]["girl_tomorrow_task_info"]["link"]
+    tomgirl.level = retData["challenge_info"]["girl_tomorrow_task_info"]["level"]
+    tomgirl.code_info = retData["challenge_info"]["girl_tomorrow_task_info"]["code"]
+
     send_data = f"今天的男方任务是:{boy.task}\r\n跳转链接:{boy.link}\r\ncodeinfo是:{boy.code_info}\r\nlevel是:{boy.level}\r\n今天的女方任务是:{girl.task}\r\n跳转链接:{girl.link}\r\ncodeinfo是:{girl.code_info}\r\nlevel是:{girl.level}"
     print(send_data)
-    send_ding_talk(boy,girl)
+
+    send_ding_talk(boy,girl,tomboy,tomgirl)
